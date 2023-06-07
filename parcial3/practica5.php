d0f45d0168e63bef4e44721030a199376b2ea189
josephEloirM <124744328+josephEloirM@users.noreply.github.com>
2023-06-06 18:48:43 -0600

    Add files via upload

--- parcial3/practica5.php
+++ parcial3/practica5.php
@@ -1,4 +1,31 @@
 <?php
-    $cuadros=$_POST["cuadro"];
-    var_dump($cuadros);
-?>
+
+    $cuadros = S_POST("cuadro");
+
+    for($i=0; $i<count($cuadros); $i++){
+        echo $cuadros($i). "."
+        if($cuadros[$i] =! "x" || $cuadros[$i] =! "O"){
+            echo "<h2>Solo se permiten X y/o O</h2>"
+            return;
+        } 
+    }
+    /=
+
+        [0][1][2]
+        [ ][ ][ ]
+        [ ][ ][ ]
+    =/
+    if($cuadros(0) =="x" && $cuadros[1] == "x" && $cuadros(2) == "x"){
+        echo"<h1>=Ganador (X)</h1>";
+        return;
+    } else if($cuadros(0) =="O" && $cuadros[1] == "O" && $cuadros(2) == "O"){
+        echo"<h1>Ganador (O)</h1>"; 
+}
+/=
+
+        [ ][ ][ ]
+        [3][4][5]
+        [ ][ ][ ]
+=/
+
+    ?>
